eb594a3bcc5f2b2bc4e904eef9f42887138f8c00
smruti rekha pradhan <smrutirekhapradhan939@gmail.com>
2026-09-13 20:25:15 +0530

    chore: restructure project and add React frontend

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
-import { app } from "./src/app.js";
-import connectDB from "./src/config/db.js";
+import { app } from "../src/app.js";
+import connectDB from "../src/config/db.js";
 import dotenv from "dotenv";
 
 const result = dotenv.config()
